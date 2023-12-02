7cca863371fefd17917975592b77e70f632ee6af
Lanznx <109305092@g.nccu.edu.tw>
2023-12-03 01:53:02 +0800

    test: add more test cases for test_member_api

--- tests/api/test_member_api.py
+++ tests/api/test_member_api.py
@@ -21,3 +21,49 @@ def test_member_creation():
     response_data = response.json()
     assert response_data["message"] == "Member created successfully"
     assert "user_id" in response_data
+
+
+def test_member_creation_with_missing_data():
+    member_data = {
+        "first_name": "John",
+        # "last_name" is missing
+        "gender": "Male",
+        "date_of_birth": str(date.today()),
+        "email": "john.doe@example.com",
+    }
+
+    response = client.post("/member/", json=member_data)
+
+    assert response.status_code == 422
+    assert "detail" in response.json()
+
+
+def test_member_creation_with_future_birth_date():
+    future_date = str(date.today().replace(year=date.today().year + 1))
+    member_data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "gender": "Male",
+        "date_of_birth": future_date,
+        "email": "john.doe@example.com",
+    }
+
+    response = client.post("/member/", json=member_data)
+
+    assert response.status_code == 422
+    assert "detail" in response.json()
+
+
+def test_member_creation_with_invalid_email():
+    member_data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "gender": "Male",
+        "date_of_birth": str(date.today()),
+        "email": "invalid-email",
+    }
+
+    response = client.post("/member/", json=member_data)
+
+    assert response.status_code == 422
+    assert "detail" in response.json()
