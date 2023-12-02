94fd9781aa292286a4c79fda7b8f4d7cacba8b37
Lanznx <109305092@g.nccu.edu.tw>
2023-12-02 21:35:53 +0800

    Refactor : MemberInputDTO and add validation

--- app/use_cases/dtos/member_dto.py
+++ app/use_cases/dtos/member_dto.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr, field_validator
 from datetime import date
 
 
 class MemberInputDTO(BaseModel):
-    first_name: str
-    last_name: str
+    first_name: constr(min_length=1)
+    last_name: constr(min_length=1)
     gender: str
     date_of_birth: date
     email: EmailStr
+
+    @field_validator("date_of_birth")
+    def check_date_of_birth(cls, value: date):
+        if value > date.today():
+            raise ValueError("Date of Birth cannot be in the future")
+        return value
--- tests/unit/use_cases/test_member_use_cases.py
+++ tests/unit/use_cases/test_member_use_cases.py
@@ -1,5 +1,6 @@
 import pytest
-from datetime import date
+import pydantic
+from datetime import datetime, timedelta, date
 from unittest.mock import Mock
 from app.entities.member import Member
 from app.use_cases.member_use_cases.member_creation import MemberCreationUseCase
@@ -40,3 +41,41 @@ def test_add_member(member_creation_use_case, member_repository_mock):
     assert new_member.gender == member_data.gender
     assert new_member.date_of_birth == member_data.date_of_birth
     assert new_member.email == member_data.email
+
+
+@pytest.mark.parametrize("email", ["invalid-email", "test@test", ""])
+def test_add_member_with_invalid_email(member_creation_use_case, email):
+    with pytest.raises(pydantic.ValidationError) as excinfo:
+        MemberInputDTO(
+            first_name="Invalid",
+            last_name="Email",
+            gender="Male",
+            date_of_birth="1990-01-01",
+            email=email,
+        )
+    assert "value is not a valid email address" in str(excinfo.value)
+
+
+def test_add_member_with_future_date(member_creation_use_case):
+    future_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+    with pytest.raises(pydantic.ValidationError) as excinfo:
+        MemberInputDTO(
+            first_name="Future",
+            last_name="Date",
+            gender="Female",
+            date_of_birth=future_date,
+            email="future.date@example.com",
+        )
+    assert "Date of Birth cannot be in the future" in str(excinfo.value)
+
+
+@pytest.mark.parametrize("first_name, last_name", [("", "Doe"), ("John", "")])
+def test_add_member_with_empty_name(member_creation_use_case, first_name, last_name):
+    with pytest.raises(pydantic.ValidationError) as excinfo:
+        MemberInputDTO(
+            first_name=first_name,
+            last_name=last_name,
+            gender="Male",
+            date_of_birth="1990-01-01",
+            email="john.doe@example.com",
+        )
