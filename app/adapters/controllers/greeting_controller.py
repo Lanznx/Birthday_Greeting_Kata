0d503800e9abaff4e98a96fd8d33c38c779c47d4
Lanznx <109305092@g.nccu.edu.tw>
2023-12-03 21:23:24 +0800

    feat: v2 birthday greeting endpoint and service implementation

--- app/adapters/controllers/greeting_controller.py
+++ app/adapters/controllers/greeting_controller.py
@@ -6,21 +6,36 @@ from app.use_cases.ports.member_repository import IMemberRepository
 from app.use_cases.ports.greeting_service import IGreetingService
 from app.infra.dependencies import get_member_repository, get_greeting_service
 from app.use_cases.dtos.birthday_greeting_dto import (
-    BirthdayGreetingInputDTOV1,
+    BirthdayGreetingInputDTO,
     BirthdayGreetingOutputDTOV1,
+    BirthdayGreetingOutputDTOV2,
 )
 
 greeting_routes = APIRouter(tags=["greeting"], prefix="/greeting")
 
 
 @greeting_routes.post("/v1/birthday", status_code=status.HTTP_200_OK)
-def send_birthday_greetings_v1(today: BirthdayGreetingInputDTOV1):
+def send_birthday_greetings_v1(today: BirthdayGreetingInputDTO):
     try:
         member_repository: IMemberRepository = get_member_repository()
         greeting_service: IGreetingService = get_greeting_service("v1")
         use_case = SendBirthdayGreetingUseCase(member_repository, greeting_service)
         greetings = use_case.execute(today.current_date)
         return BirthdayGreetingOutputDTOV1(
+            message="Birthday greetings sent successfully", greetings=greetings
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@greeting_routes.post("/v2/birthday", status_code=status.HTTP_200_OK)
+def send_birthday_greetings_v2(today: BirthdayGreetingInputDTO):
+    try:
+        member_repository: IMemberRepository = get_member_repository()
+        greeting_service: IGreetingService = get_greeting_service("v2")
+        use_case = SendBirthdayGreetingUseCase(member_repository, greeting_service)
+        greetings = use_case.execute(today.current_date)
+        return BirthdayGreetingOutputDTOV2(
             message="Birthday greetings sent successfully", greetings=greetings
         )
     except Exception as e:
