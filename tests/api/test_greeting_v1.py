4e97fc21d51f8dbf67fce470d5a27030bed5678d
Lanznx <109305092@g.nccu.edu.tw>
2023-12-03 22:57:04 +0800

    Refactor: birthday greeting tests and add v3 test

--- tests/api/test_greeting_v1.py
+++ tests/api/test_greeting_v1.py
@@ -1,22 +1,22 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from datetime import date
 
 client = TestClient(app)
 
 
 def test_send_birthday_greetings_v1():
-    today = date.today()
-    request_data = {"current_date": today.isoformat()}
+    request_data = {"current_date": "2023-08-08"}
 
     response = client.post("/greeting/v1/birthday", json=request_data)
 
-    assert response.status_code == 200
-    response_data = response.json()
-    assert response_data["message"] == "Birthday greetings sent successfully"
-    assert isinstance(response_data["greetings"], list)
-    if response_data["greetings"]:
+    if response.status_code == 200:
+        response_data = response.json()
+        assert response_data["message"] == "Birthday greetings sent successfully"
+        assert isinstance(response_data["greetings"], list)
         greeting = response_data["greetings"][0]
         assert "recipient_email" in greeting
         assert "subject" in greeting and greeting["subject"] == "Happy birthday!"
         assert "message" in greeting
+    elif response.status_code == 404:
+        response_data = response.json()
+        assert response_data["detail"] == "No birthdays found today."
--- tests/api/test_greeting_v2.py
+++ tests/api/test_greeting_v2.py
@@ -1,21 +1,18 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from datetime import date
 
 client = TestClient(app)
 
 
 def test_send_birthday_greetings_v2():
-    today = date.today()
     request_data = {"current_date": "2023-08-08"}
 
     response = client.post("/greeting/v2/birthday", json=request_data)
 
-    assert response.status_code == 200
-    response_data = response.json()
-    assert response_data["message"] == "Birthday greetings sent successfully"
-    assert isinstance(response_data["greetings"], list)
-    if response_data["greetings"]:
+    if response.status_code == 200:
+        response_data = response.json()
+        assert response_data["message"] == "Birthday greetings sent successfully"
+        assert isinstance(response_data["greetings"], list)
         greeting = response_data["greetings"][0]
         assert "recipient_email" in greeting
         assert "subject" in greeting
@@ -25,3 +22,6 @@ def test_send_birthday_greetings_v2():
             assert "20% off" in greeting["message"]
         elif "Cosmetic, LV Handbags" in greeting["message"]:
             assert "50% off" in greeting["message"]
+    elif response.status_code == 404:
+        response_data = response.json()
+        assert response_data["detail"] == "No birthdays found today."
